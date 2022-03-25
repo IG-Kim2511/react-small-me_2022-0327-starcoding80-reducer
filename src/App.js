f189168d61e7c19c645f1d53957c4e798ceafd64
IG <iikim2511@gmail.com>
2022-03-25 16:17:29 -0400

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 
-function App() {
+// {/*  */}
+
+/* 🍀 js0409. use reducer
+
+🍉10
+  useReducer : useState와 비슷하게 variable의 한 형태
+
+  1번 파라미터 : reducer함수를 통해 state를 바꿈
+
+  2번 파라미터 : state의 초기값
+
+  state와 다를거 없이 variable로써 활용함
+
+
+  🍉20
+  reducer함수의
+
+  1번 파라미터 : state : useReducer의 현재 state를 받음
+
+  2번파라미터 : action : payload를 통해 바뀜. (= usState의  set~~)
+
+  🍉30
+  useState처럼 변수로 쓰임. 즉 변수의 한 형태
+
+  🍉40
+  dispatch = useState의 set~~ 과 같은방식임 - state를 바꿔주는 함수 
+*/
+
+  // js0409-20.
+const reducer = (state, action)=>{
+  console.log('execute reducer',state, action)
+};
+
+
+function App() {  
+  // 🍀 js0404. input value
   const [number, setNumber] = useState(0);
+
+
+  // js0409-10
+  const [stateMoney, dispatch] = useReducer(reducer, 0);
 
   return (
     <div>
       <h2>useReducer Bank</h2>
 
-      <p>account : ? $</p>
 
+      {/* js0409-30.*/}
+      <p>account : {stateMoney} $</p>
+
+      {/* js0404.*/}
       <input
         type="number"
         value={number}
@@ -17,7 +59,14 @@ function App() {
         }}
       />
 
-      <button>Deposit</button>
+      {/* js0409-40.*/}
+      <button onClick={()=>{
+        dispatch({type: 'deposit'});
+      }}>Deposit</button>
+
+
+
+      
       <button>withdraw</button>
 
     </div>
